bd4b65d90e150b904eb2d44bfbfad73113cfde5b
adamteddychang <adamchang0725@gmail.com>
2022-04-25 10:06:50 +0200

    added proptypes and linters fixed

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -1,18 +1,21 @@
-import './project.css'
+import './project.css';
+import PropTypes from 'prop-types';
 
-const Project = ({img,link}) => {
-  return (
-    <div className='p'>
-      <div className="p-browser">
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-        <div className="p-circle"></div>
-      </div>
-      <a href={link} target="_blank" rel="noreferrer">
-        <img src={img} alt="" className="p-img" />
-      </a>
+const Project = ({ img, link }) => (
+  <div className="p">
+    <div className="p-browser">
+      <div className="p-circle" />
+      <div className="p-circle" />
+      <div className="p-circle" />
     </div>
-  )
-}
+    <a href={link} target="_blank" rel="noreferrer">
+      <img src={img} alt="" className="p-img" />
+    </a>
+  </div>
+);
 
+Project.propTypes = {
+  link: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+};
 export default Project;
--- src/components/projectsList/projectsList.jsx
+++ src/components/projectsList/projectsList.jsx
@@ -1,31 +1,27 @@
-import Project from '../project/project'
-import './projectsList.css'
-import projects from '../../data'
+import Project from '../project/project';
+import './projectsList.css';
+import projects from '../../data';
 
-const ProjectsList = () => {
-  projects.forEach((element) => {
-    console.log(element.link); 
-});
+const ProjectsList = () => (
+  <div className="pl">
+    <div className="pl-texts">
 
-  return (
-    <div className='pl'>
-      <div className="pl-texts">
+      <h1 className="pl-title">Create and Inspire</h1>
+      <p className="pl-desc">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Quam accusantium enim a optio cum porro odio eius vel,
+        totam et error itaque laudantium ullam aliquam eum facere cupiditate in iure.
+      </p>
 
-        <h1 className="pl-title">Create and Inspire</h1>
-        <p className="pl-desc">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam accusantium enim a optio cum porro odio eius vel, totam et error itaque laudantium ullam aliquam eum facere cupiditate in iure.
-        </p>
-
-      </div>
-      <div className="pl-list">
-        {projects.map((proj) => (
-          <Project key={proj.id} img={proj.img} link={proj.link} />
-
-        ))}
-      </div>
     </div>
+    <div className="pl-list">
+      {projects.map((proj) => (
+        <Project key={proj.id} img={proj.img} link={proj.link} />
 
-  );
-};
+      ))}
+    </div>
+  </div>
 
-export default ProjectsList
+);
+
+export default ProjectsList;
