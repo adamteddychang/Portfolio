48139744b278d4065037b060b02112ecb7ba5135
adamteddychang <adamchang0725@gmail.com>
2022-04-26 10:14:03 +0200

    added toggle button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import About from './components/about/About';
 import Contact from './components/contact/contact';
 import Intro from './components/intro/Intro';
 import ProjectsList from './components/projectsList/projectsList';
+import Toggle from './components/toggle/Toggle';
 
 function App() {
   return (
     <div>
+      <Toggle />
       <Intro />
       <About />
       <ProjectsList />
       <Contact />
+      
     </div>
   );
 }
